692c211a8620a5a97aed114cabd32f2c26ac70c3
Logan <37604784+imlogang@users.noreply.github.com>
2025-12-09 21:15:23 -0600

    Update discord that the server(s) have been updated. Make update_power_sever able to take input to change what state the server is in. 2 functions had the same name. Cleanup the data payload

--- update.sh
+++ update.sh
@@ -16,9 +16,7 @@ function notify_minecraft_server() {
         -H "Accept: application/json" \
         -H "Content-Type: application/json" \
         -H "Authorization: Bearer ${PteroToken}" \
-        -d "{
-            \"command\": \"say ${message}\"
-        }"
+        -d "{\"command\": \"say ${message}\"}"
 }
 
 function update_discord_and_server() {
@@ -35,15 +33,17 @@ function update_discord_and_server() {
     notify_discord "The server will be updated to version ${STABLE_VERSION} in 5 seconds. Please update your client!"
     notify_minecraft_server "The server will be updating in 5 seconds to ${STABLE_VERSION}!"
     sleep 5
+    notify_discord "The server is now updated to version ${STABLE_VERSION}."
 }
 
-function update_discord_and_server() {
+function update_discord() {
     notify_discord "The server will be updated in 5 minutes. Please update your client!"
     sleep 4m 
     notify_discord "The server will be updated in 1 minute. Please update your client!"
     sleep 55
     notify_discord "The server will be updated in 5 seconds. Please update your client!"
     sleep 5
+    notify_discord "The server is now updated"
 }
 
 function update_reinstall_server() {
@@ -54,15 +54,14 @@ function update_reinstall_server() {
         -H "Authorization: Bearer ${PteroToken}"
 }
 
-function update_restart_server() {
-    echo "Updating server by reinstalling"
+function update_power_server() {
+    local power_state="$1"
+    echo "Updating server by ${power_state}"
     curl -s -X POST "http://minecraftdell2.logangodsey.com/api/client/servers/${serverValue}/power" \
         -H "Accept: application/json" \
         -H "Content-Type: application/json" \
         -H "Authorization: Bearer ${PteroToken}" \
-        -d '{
-            "signal": "restart"
-        }'
+        -d '{"signal": "'"$power_state"'"}'
 }
 
 
@@ -75,13 +74,14 @@ case "$serverValue" in
     STABLE_VERSION=$(curl -s https://piston-meta.mojang.com/mc/game/version_manifest_v2.json | jq -r '.latest.release')
     update_discord_and_server
     update_reinstall_server
+    update_power_server "start"
     ;;
 
   "6b774df5")
     echo "Running commands Satisfactory"
     export DISCORD_WEBHOOK_LINK="${DISCORD_WEBHOOK_LINK_SATISFACTORY}"
-    update_discord_and_server
-    update_restart_server
+    update_discord
+    update_power_server "restart"
     ;;
   *)
     echo "Unknown serverValue: $serverValue"
